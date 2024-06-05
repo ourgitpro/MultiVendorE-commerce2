c2f257a39a93bac2e112862d0cb6a6001e8de959
Md. Nahid Hossain Eblo <56239953+ourgitpro@users.noreply.github.com>
2024-06-05 08:14:41 -0700

    backend work

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const app = express();
+
 require("dotenv").config();
+const dbConnect = require("./utiles/dbConnect");
+//const { dbConnect } = require("./utiles/dbConnect");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+dbConnect();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/api", require("./routes/authRoutes"));
 app.get("/", (req, res) => res.send("My Backend"));
-const port = process.env.PORT;
-app.listen(port, () => console.log("server is running on port 5000"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Server is running ${port}`));
